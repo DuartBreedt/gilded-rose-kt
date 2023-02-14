46f1a8ee995303d17d470ace3f206ff6a4c05606
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2023-02-14 16:43:27 +0200

    Added Gilded Rose Kotlin project

---
+++ src/test/kotlin/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,15 @@
+package gildedrose
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class GildedRoseTest {
+
+    @Test
+    fun foo() {
+        val items = arrayOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("fixme", app.items[0].name)
+    }
+}
