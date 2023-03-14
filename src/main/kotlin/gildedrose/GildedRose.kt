f4802f1389fbe2e1b1a7e58d8463a6455bbf8e32
Duart Breedt <duart.breedt@entelect.co.za>
2023-03-14 10:20:33 +0200

    Reverted Tania's logic

--- src/main/kotlin/gildedrose/GildedRose.kt
+++ src/main/kotlin/gildedrose/GildedRose.kt
@@ -5,24 +5,26 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             when (items[i]) {
-                is AgedBrieItem -> {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                is AgedBrieItem -> {}
 
-                    items[i].sellIn = items[i].sellIn - 1
+                is BackstageItem -> {}
 
-                    if (items[i].sellIn < 0) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
+                is SulfurasItem -> {}
+
+                is ConjuredItem -> {}
+
+                else -> {}
+            }
+
+            if (items[i] !is AgedBrieItem && items[i] !is BackstageItem && items[i] !is SulfurasItem) {
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1
                 }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
 
-                is BackstageItem -> {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-
+                    if (items[i] is BackstageItem) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -35,57 +37,27 @@ class GildedRose(var items: Array<Item>) {
                             }
                         }
                     }
+                }
+            }
 
-                    items[i].sellIn = items[i].sellIn - 1
+            if (items[i] !is SulfurasItem) {
+                items[i].sellIn = items[i].sellIn - 1
+            }
 
-                    if (items[i].sellIn < 0) {
+            if (items[i].sellIn < 0) {
+                if (items[i] !is AgedBrieItem) {
+                    if (items[i] !is BackstageItem) {
+                        if (items[i].quality > 0) {
+                            if (items[i] !is SulfurasItem) {
+                                items[i].quality = items[i].quality - 1
+                            }
+                        }
+                    } else {
                         items[i].quality = items[i].quality - items[i].quality
                     }
-                }
-
-                is SulfurasItem -> {
+                } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
-                    }
-
-                    if (items[i].sellIn < 0) {
-                        if (items[i].quality > 0) {
-                            if (items[i] !is SulfurasItem) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    }
-                }
-
-                is ConjuredItem -> {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-
-                    items[i].sellIn = items[i].sellIn - 1
-
-                    if (items[i].sellIn < 0) {
-                        if (items[i].quality > 0) {
-                            if (items[i] !is SulfurasItem) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    }
-                }
-
-                else -> {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1
-                    }
-
-                    items[i].sellIn = items[i].sellIn - 1
-
-                    if (items[i].sellIn < 0) {
-                        if (items[i].quality > 0) {
-                            if (items[i] !is SulfurasItem) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
                     }
                 }
             }
